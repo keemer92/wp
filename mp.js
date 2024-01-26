42fceaf06589908876fd57c07ff995bacc395786
Waquiem Comar <wcomar92@gmail.com>
2024-01-26 02:38:29 -0500

    put the old version back until the new version is upddated with maintenance and fatloss

--- mp.js
+++ mp.js
@@ -1,13 +1,11 @@
-const prompt = require("prompt-sync")(); //modules that need to be installed
+const prompt = require("prompt-sync")();
 console.log("Time to get your dream body");
-//const firstName = prompt("What is your first name? ");
-//const lastName = prompt("What is your last name? ");
-// email = prompt("What is your email address? ");
-const weight = Number(prompt("How much do you weight in pounds? "));
+
+const weight = Number(prompt("How much do you weigh in pounds? "));
 const fatPercentage = Number(prompt("What is your body fat percentage? "));
 const goal = Number(prompt("Type 1 for muscle growth (weight gain). Type 2 for fat loss (shred/tone-up). Type 3 for maintenance: "));
 
-let selectedMuscle; // the const keyword was giving errors
+let selectedMuscle;
 if (goal === 1) {
     selectedMuscle = prompt("Choose one of the following muscles to grow over the next 6 weeks: Chest, Back, Shoulders, Triceps, Biceps, Glutes, Hamstrings, Quads, Calves: ");
 }
@@ -21,11 +19,7 @@ const workoutDays = {
     Tuesday: [sets, growthOrMaintenanceReps, 'Squats', 'Stiff Leg Deadlifts', 'Adductor', 'Abductor', 'Hamstring Curls', 'Leg Extensions', 'Seated Calf Raises', 'Standing Calf Raises'],
     Wednesday: 'Rest',
     Thursday: [pumpReps1, 'Bench Press', 'Incline Bench Press', 'Chest Fly', sets, growthOrMaintenanceReps, 'Shoulder Press', 'JM Press', 'Barbell Rows', 'Lat Pulldown', 'Barbell Shrugs', 'Barbell Face Pull', 'Barbell Upright Rows', 'Barbell Bicep Curls'],
-    Thursdayfatloss: [pumpReps1, 'Bench Press', 'Incline Bench Press', 'Chest Fly', 'Shoulder Press', 'JM Press', 'Barbell Rows', 'Lat Pulldown', 'Barbell Shrugs', 'Barbell Face Pull', 'Barbell Upright Rows', 'Barbell Bicep Curls'],
-    Thursdaymaintenance: [sets, growthOrMaintenanceReps, 'Bench Press', 'Incline Bench Press', 'Chest Fly', 'Shoulder Press', 'JM Press', 'Barbell Rows', 'Lat Pulldown', 'Barbell Shrugs', 'Barbell Face Pull', 'Barbell Upright Rows', 'Barbell Bicep Curls'],
     Friday: [sets, growthOrMaintenanceReps, 'Squats', 'Stiff Leg Deadlifts', 'Adductor', 'Abductor', 'Hamstring Curls', 'Leg Extensions', 'Seated Calf Raises', 'Standing Calf Raises'],
-    Fridayfatloss: [pumpReps1, 'Squats', 'Stiff Leg Deadlifts', 'Adductor', 'Abductor', 'Hamstring Curls', 'Leg Extensions', 'Seated Calf Raises', 'Standing Calf Raises'],
-    
 };
 
 const muscleGroups = {
@@ -63,7 +57,7 @@ function generateWorkout(muscle) {
     return workout;
 }
 
-if (goal === 1 && fatPercentage < 25) {
+if (goal === 1) {
     const userWorkout = generateWorkout(selectedMuscle);
     console.log(userWorkout);
 
@@ -91,24 +85,3 @@ if (goal === 1 && fatPercentage < 25) {
 } else {
     console.log("Unsupported goal or no muscle selected for muscle growth.");
 }
-
-const fatlossWorkout = {}; // Initialize the workout object
-
-if (goal === 2 && fatPercentage >= 25) {
-    fatlossWorkout.Monday = [...workoutDays.Monday];
-    fatlossWorkout.Tuesday = [...workoutDays.Tuesday];
-    fatlossWorkout.Wednesday = [...workoutDays.Wednesday];
-    fatlossWorkout.Thursday = [...workoutDays.Thursdayfatloss];
-    fatlossWorkout.Friday = [...workoutDays.Fridayfatloss];
-} console.log(fatlossWorkout);
-
-const maintenanceWorkout = {};
-
-//all of these days should have the growthor maintenance reps
-if (goal === 3) {
-    maintenanceWorkout.Monday = [...workoutDays.Monday];
-    maintenanceWorkout.Tuesday = [...workoutDays.Tuesday];
-    maintenanceWorkout.Wednesday = [...workoutDays.Wednesday];
-    maintenanceWorkout.Thursday = [...workoutDays.Thursdaymaintenance];
-    maintenanceWorkout.Friday = [...workoutDays.Friday];
-} console.log(maintenanceWorkout);
